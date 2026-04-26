e93de7b28fea6ad25e9f07c64e1c326ad3a064ee
zerodmun <munirakromin@gmail.com>
2026-04-26 19:38:19 +0700

    Configure Supabase cloud store

--- cloud-config.js
+++ cloud-config.js
@@ -1,7 +1,7 @@
 window.StoreCloudConfig = {
   provider: "supabase",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://qhaxhpafdmswcltdnwqs.supabase.co",
+  supabaseAnonKey: "sb_publishable_0qfhQjxRQ6LgGbJXWPUYlg_IVeb_zkP",
   table: "site_store",
   rowId: "threadline",
 };
